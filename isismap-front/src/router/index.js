5208e4ceaa4f735122a3bd0e867ac20ed483c8d3
Sfayhim <mahdiabdelrahmen.sfeyhi5945@istic.ucar.tn>
2026-04-10 01:59:09 +0200

    ok

--- isismap-front/src/router/index.js
+++ isismap-front/src/router/index.js
@@ -1,31 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import MapView from '../views/MapView.vue'
-import AdminView from '../views/AdminView.vue'
-import StatsView from '../views/StatsView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import MapView from "../views/MapView.vue";
+import AdminView from "../views/AdminView.vue";
+import StatsView from "../views/StatsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      redirect: '/map' // Notre fameuse redirection directe
+      path: "/",
+      redirect: "/map", // Notre fameuse redirection directe
     },
     {
-      path: '/map',
-      name: 'map',
-      component: MapView
+      path: "/map",
+      name: "map",
+      component: MapView,
     },
     {
-      path: '/stats',
-      name: 'stats',
-      component: StatsView
+      path: "/stats",
+      name: "stats",
+      component: StatsView,
     },
     {
-      path: '/admin',
-      name: 'admin',
-      component: AdminView
-    }
-  ]
-})
+      path: "/admin",
+      name: "admin",
+      component: AdminView,
+    },
+  ],
+});
 
-export default router
+export default router;
